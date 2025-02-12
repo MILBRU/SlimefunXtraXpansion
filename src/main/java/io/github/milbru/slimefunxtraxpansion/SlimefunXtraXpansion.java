39bdbcc0ece82983a241938f5b4e22f99467347e
MILBRU <155975321+MILBRU@users.noreply.github.com>
2025-02-12 19:17:38 +0100

    Update SlimefunXtraXpansion.java

--- src/main/java/io/github/milbru/slimefunxtraxpansion/SlimefunXtraXpansion.java
+++ src/main/java/io/github/milbru/slimefunxtraxpansion/SlimefunXtraXpansion.java
@@ -4,9 +4,9 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import io.github.thebusybiscuit.slimefun4.implementation.SlimefunItems;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
